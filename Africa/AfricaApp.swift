b86f4d11a57e1cfacc06eabf4888ded59e77a9d7
Nonato Sousa <nonato.sousa@antikytera.com>
2023-10-04 06:55:47 -0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Nonato Sousa on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
